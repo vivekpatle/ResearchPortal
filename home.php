63386503cd4b1726341aa0490756d57d85c2f69a
Vivek Patle <vivekpatle@outlook.in>
2017-04-15 16:14:35 +0530

    login updated

--- home.php
+++ home.php
@@ -37,7 +37,7 @@
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="#">
-                    <img src="images/navbar/ducolorlogonew.png" alt="logo" height="60px" width="60px" >
+                    <img src="images/navbar/ducolorlogonew.png" alt="logo" height="60px" width="60px">
                 </a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
@@ -52,10 +52,10 @@
                     <li>
                         <a href="research_profile.php" id="jake" class="hvr-underline-from-center"> <i class="fa fa-book" aria-hidden="true"></i>&nbsp Research Profile
                         </a>
-                    <li>
-                           &nbsp &nbsp <button class="btn btn-primary" data-toggle="modal" data-target="#myModal" id="jake"> Login </button>
+                        <li>
+                            &nbsp &nbsp <button class="btn btn-primary" data-toggle="modal" data-target="#myModal" id="jake"> Login </button>
 
-                    </li>
+                        </li>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
@@ -63,48 +63,123 @@
         </div>
         <!-- /.container -->
     </nav>
-<!--Carousel-->
+    <!---Login Popup-->
+    <form action="login.php" method="post">
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                        <h4 class="modal-title" id="myModalLabel" style="font-family: 'Slabo 27px', serif; font-size: 1.5em; font-style: bold;"> <i class="fa fa-sign-in" aria-hidden="true"></i> Log in </h4>
+                    </div>
+                    <!-- /.modal-header -->
+
+                    <div class="modal-body">
+                        <form role="form">
+                            <div class="form-group">
+                                <label class="radio-inline">
+                <input type="radio" name="genre" id="inlineCheckbox1" value="student"  />
+                Student
+            </label>
+                                <label class="radio-inline">
+                <input type="radio" name="genre" id="inlineCheckbox2" value="teacher" />
+                Faculty
+            </label>
+                                <br>
+                                <br>
+                                <div class="input-group">
+                                    <input type="text" class="form-control" id="uLogin" name="username" placeholder="Enrollment Number">
+                                    <label for="uLogin" class="input-group-addon glyphicon glyphicon-user"></label>
+                                </div>
+                            </div>
+                            <!-- /.form-group -->
+
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <input type="password" class="form-control" id="uPassword" name="password" placeholder="Password">
+                                    <label for="uPassword" class="input-group-addon glyphicon glyphicon-lock"></label>
+
+
+
+
+
+
+                                    </p>
+                                </div>
+                                <!-- /.input-group -->
+                            </div>
+                            <!-- /.form-group -->
+
+
+                        </form>
+
+                    </div>
+                    <!-- /.modal-body -->
+
+                    <div class="modal-footer">
+                        <button class="form-control btn-primary btn-lg btn-block" id="jake">Submit</button>
+                        <br>
+                        <a href="signup.php">New user? Register Here</a>
+                        <div class="progress">
+                            <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="1" aria-valuemin="1" aria-valuemax="100" style="width: 0%;">
+                                <span class="sr-only">progress</span>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.modal-footer -->
+
+                </div>
+                <!-- /.modal-content -->
+            </div>
+            <!-- /.modal-dialog -->
+        </div>
+        <!-- /.modal -->
+    </form>
+
+
+    <!--Carousel-->
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-    <li data-target="#myCarousel" data-slide-to="3"></li>
-  </ol>
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+        </ol>
 
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-      <img src="images/carousel/2.jpg" alt="Chania">
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="images/carousel/2.jpg" alt="Chania">
+            </div>
+
+            <div class="item">
+                <img src="images/carousel/3.jpg" alt="Chania">
+            </div>
+
+            <div class="item">
+                <img src="images/carousel/4.jpg" alt="Flower">
+            </div>
+
+            <div class="item">
+                <img src="images/carousel/5.jpg" alt="Flower">
+            </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
+    <!--.carousel-->
 
-    <div class="item">
-      <img src="images/carousel/3.jpg" alt="Chania">
-    </div>
-
-    <div class="item">
-      <img src="images/carousel/4.jpg" alt="Flower">
-    </div>
-
-    <div class="item">
-      <img src="images/carousel/5.jpg" alt="Flower">
-    </div>
-  </div>
-
-  <!-- Left and right controls -->
-  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-<!--.carousel-->
-
-<!--Social Media-->
+    <!--Social Media-->
 
     <div style="background-color:#383A56; padding:30px">
         <div>
@@ -139,7 +214,7 @@
                 </div>
                 <div class="col-lg-4">
                     <div class="jumbotron" style="padding: 10px">
-                        <h4 style="text-align:center"><i class="fa fa-twitter" aria-hidden="true"></i>&nbsp;  Twitter</h4>
+                        <h4 style="text-align:center"><i class="fa fa-twitter" aria-hidden="true"></i>&nbsp; Twitter</h4>
                         <h5 style="text-align:center">November 4, 2016</h5>
                         <hr class="my-2">
                         <p style="text-align:center; font-family: 'Taviraj', serif;">#DelhiUniversity | FMS Delhi admissions 2017: Application process for MBA and Ph D course to end before CAT 2016 </p>
@@ -153,7 +228,7 @@
     </div>
 
     <!--.Social Media-->
-<div id="footerwrap">
+    <div id="footerwrap">
         <div class="container">
             <div class="row">
                 <div class="col-lg-4">
@@ -188,56 +263,59 @@
     </div>
 
 
-<script>
-$(document).ready(function(){
-    $('.modal-footer button').click(function(){
-        var button = $(this);
-        if ( button.attr("data-dismiss") != "modal" ){
-            var inputs = $('form input');
-            var title = $('.modal-title');
-            var progress = $('.progress');
-            var progressBar = $('.progress-bar');
-            inputs.attr("disabled", "disabled");
-            button.hide();
-            progress.show();
-            progressBar.animate({width : "100%"}, 100);
-            progress.delay(1000)
-                    .fadeOut(600);
-            button.text("Close")
-                    .removeClass("btn-primary")
-                    .addClass("btn-success")
-                    .blur()
-                    .delay(1600)
-                    .fadeIn(function(){
-                        title.text("Log in is successful");
-                        button.attr("data-dismiss", "modal");
-                    });
-        }
-    });
-    $('#myModal').on('hidden.bs.modal', function (e) {
-        var inputs = $('form input');
-        var title = $('.modal-title');
-        var progressBar = $('.progress-bar');
-        var button = $('.modal-footer button');
-        inputs.removeAttr("disabled");
-        title.text("Log in");
-        progressBar.css({ "width" : "0%" });
-        button.removeClass("btn-success")
-                .addClass("btn-primary")
-                .text("Ok")
-                .removeAttr("data-dismiss");
-                
-    });
-});
-</script>
+    <script>
+        $(document).ready(function() {
+            $('.modal-footer button').click(function() {
+                var button = $(this);
+                if (button.attr("data-dismiss") != "modal") {
+                    var inputs = $('form input');
+                    var title = $('.modal-title');
+                    var progress = $('.progress');
+                    var progressBar = $('.progress-bar');
+                    inputs.attr("disabled", "disabled");
+                    button.hide();
+                    progress.show();
+                    progressBar.animate({
+                        width: "100%"
+                    }, 100);
+                    progress.delay(1000)
+                        .fadeOut(600);
+                    button.text("Close")
+                        .removeClass("btn-primary")
+                        .addClass("btn-success")
+                        .blur()
+                        .delay(1600)
+                        .fadeIn(function() {
+                            title.text("Log in is successful");
+                            button.attr("data-dismiss", "modal");
+                        });
+                }
+            });
+            $('#myModal').on('hidden.bs.modal', function(e) {
+                var inputs = $('form input');
+                var title = $('.modal-title');
+                var progressBar = $('.progress-bar');
+                var button = $('.modal-footer button');
+                inputs.removeAttr("disabled");
+                title.text("Log in");
+                progressBar.css({
+                    "width": "0%"
+                });
+                button.removeClass("btn-success")
+                    .addClass("btn-primary")
+                    .text("Ok")
+                    .removeAttr("data-dismiss");
 
-<!-- jQuery -->
+            });
+        });
+    </script>
+
+    <!-- jQuery -->
     <script src="js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script> 
+    <script src="js/bootstrap.min.js"></script>
 
 </body>
+
 </html>
-
-
